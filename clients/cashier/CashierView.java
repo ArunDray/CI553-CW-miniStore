bbc09b65c780888ff89fce5e4d9b58c1930c3906
Arun <Arun@DESKTOP-CCKH9JS>
2025-01-14 03:06:54 +0000

    Updated changes for Cashier Background colour

--- clients/cashier/CashierView.java
+++ clients/cashier/CashierView.java
@@ -57,6 +57,7 @@ public class CashierView implements Observer
     Container cp         = rpc.getContentPane();    // Content Pane
     Container rootWindow = (Container) rpc;         // Root Window
     cp.setLayout(null);                             // No layout manager
+    cp.setBackground(new Color(102, 102, 102)); 	// Setting a dark grey background
     rootWindow.setSize( W, H );                     // Size of Window
     rootWindow.setLocation( x, y );
 
