8d3ba287903263ab264b71b982d86d376ce88ef3
Arun <Arun@DESKTOP-CCKH9JS>
2025-01-18 02:32:23 +0000

    Utils class created for themes

--- clients/Main.java
+++ clients/Main.java
@@ -14,6 +14,7 @@ import clients.packing.PackingModel;
 import clients.packing.PackingView;
 import middle.LocalMiddleFactory;
 import middle.MiddleFactory;
+import utils.StyleHelper;
 
 import javax.swing.*;
 import java.awt.*;
